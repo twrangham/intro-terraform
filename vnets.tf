21dda60abfd123caa100bcc448f398c9978fda45
Travis Wrangham <twrangham@gmail.com>
2022-10-14 08:42:08 -0400

    fixed cidr

--- vnets.tf
+++ vnets.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "vnet1" {
 # Create vnet2
 resource "azurerm_virtual_network" "vnet2" {
   name                = "myTFVnet2"
-  address_space       = ["10.1.0/16"]
+  address_space       = ["10.1.0.0/16"]
   location            = "eastus"
   resource_group_name = azurerm_resource_group.rg1.name
 }
