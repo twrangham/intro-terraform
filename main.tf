2cd342cf928349adc034c2e462ea58fdd24b37f6
Travis Wrangham <twrangham@gmail.com>
2022-10-14 08:25:21 -0400

    removed vnet from main

--- main.tf
+++ main.tf
@@ -47,12 +47,3 @@ resource "azurerm_resource_group" "rg3" {
     Env       = "Prod"
   }
 }
-
-# Create a virtual network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "myTFVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "eastus"
-  resource_group_name = azurerm_resource_group.rg1.name
-}
-
---
+++ vnets.tf
@@ -0,0 +1,40 @@
+# Configure the Azure provider!!!
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  # Store state in Terraform Cloud
+  cloud {
+    organization = "Wrangham1"
+    workspaces {
+      name = "intro-terraform"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create vnet1
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "myTFVnet1"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg1.name
+}
+
+# Create vnet2
+resource "azurerm_virtual_network" "vnet2" {
+  name                = "myTFVnet2"
+  address_space       = ["10.1.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg1.name
+}
+
+
